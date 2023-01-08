3164e3e9e0d0e113dda7814c831a73ed60577272
KiranJungGurung <kirangurung394@gmail.com>
2023-01-08 16:42:36 +0545

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Kiran Gurung on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
